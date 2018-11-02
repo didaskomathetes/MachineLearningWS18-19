e470f3a8857679ac98f2113f1817446373e50ec6
Samael <didaskomathetes@github.com>
2018-11-02 13:02:22 +0100

    Initial commit

---
+++ Blatt4/aufgabe3.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def main():
+    print(X(np.array([3, 2, 4, 8]), 3))
+
+
+def X(vector, d):
+    vlen = vector.size
+    X = []
+
+    for i in range(vlen):
+        vali = vector[i]
+        Xi = []
+        for j in range(d + 1):
+            Xi.append(vali ** j)
+        X.insert(i, np.array(Xi))
+    return np.array(X)
