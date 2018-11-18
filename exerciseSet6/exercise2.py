6a7e586c00cfcb796423400fefe665f70612ba16
didaskomathetes <didaskomathetes@github.com>
2018-11-18 18:46:58 +0100

    changed something

--- exerciseSet6/exercise2.py
+++ exerciseSet6/exercise2.py
@@ -16,6 +16,7 @@ def fit(X, y):
 def main():
     y = getLabels('data/svm_labels.csv')
     X = getDatapoints('data/svm_data.csv', array=1)
+    print(X.shape)
     print(X)
     result = fit(X, y)
 
