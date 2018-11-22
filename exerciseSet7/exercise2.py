5d2edcb41cdc4ccafb8dac8c59ecbcfed03dce1d
LivingTown <44706658+LivingTown@users.noreply.github.com>
2018-11-22 17:21:46 +0100

    added exersiceset 7 and file

---
+++ exerciseSet7/exercise2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#from utils.data import getDatapoints
+
+def main():
+    for n in range(1, 10):
+        print(get_eig(n)[0])
+
+
+def get_eig(n):
+    matrix = 1/n * np.ones((n, n)) + np.eye(n)
+
+    return np.linalg.eig(matrix)
+
