472b46ef6531bcaa0db70cb8eb67064cd69756ad
didaskomathetes <didaskomathetes@github.com>
2018-12-02 16:11:55 +0100

    changed structure of repo and started refactoring pca

--- exerciseSet8/exercise8_1.py
+++ exerciseSet8/exercise8_1.py
@@ -1,8 +1,8 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-def main():
+
+def main_old():
     X=constructDataMatrix()
     print(f"DataT:\n{X}")
     C = np.dot(X, X.transpose())
@@ -15,13 +15,33 @@ def main():
     fig = plt.figure()
     V  = np.array([X[0], X[1]])
     print(f"\nDatapoints by PCs, column 1 (EV:{a[0]}) and 2 (EV:{a[1]}): \n{V.transpose()}\n")
-    ax=fig.add_subplot(221, title="2d Graph of col 1 and 2")
+    ax = fig.add_subplot(221, title="2d Graph of col 1 and 2 OLD")
     ax.scatter(X[0],X[1])
 
-    ax = fig.add_subplot(222, projection='3d',title="3d Graph of col 1,2 and 4")
+    ax = fig.add_subplot(222, projection='3d', title="3d Graph of col 1,2 and 4 OLD")
     ax.scatter(X[0],X[1],X[3])
     plt.show()
 
+
+def main():
+    X = constructDataMatrix()
+    print(f"DataT:\n{X}")
+    C = np.dot(X, X.transpose())
+    print(f"\nCovariance Matrix of river flow Data:\n{C}")
+
+    a, b = np.linalg.eig(C)
+    print(f"\nEigenvalues of Covariance Matrix of River flow Data:\n{a}")
+    print(f"\nEigenvectors of Covariance Matrix of River flow Data:\n{b}")
+
+    fig = plt.figure()
+    V = np.array([X[0], X[1]])
+    print(f"\nDatapoints by PCs, column 1 (EV:{a[0]}) and 2 (EV:{a[1]}): \n{V.transpose()}\n")
+    ax = fig.add_subplot(221, title="2d Graph of col 1 and 2")
+    ax.scatter(X[0], X[1])
+
+    ax = fig.add_subplot(222, projection='3d', title="3d Graph of col 1,2 and 4")
+    ax.scatter(X[0], X[1], X[3])
+    plt.show()
 
 
 def constructDataMatrix():
